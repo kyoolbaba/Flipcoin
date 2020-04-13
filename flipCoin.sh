39e4d5e44e2b632da157b008cd403850cf8f49d7
kyoolbaba <kyoolbaba@gmail.com>
2020-04-14 01:40:33 +0530

    add triplets combination of Head/tails and its percentage

--- flipCoin.sh
+++ flipCoin.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 echo Enter the number of flips
 read flips
-declare  -A singlet[HEAD]=0
-declare -A  singlet[TAIL]=0
-declare -A doublet[HH]=0
-declare -A doublet[HT]=0
-declare -A doublet[TH]=0
-declare -A doublet[TT]=0
-declare -A percentage[HH]=0
-declare -A percentage[HT]=0
-declare -A percentage[TH]=0
-declare -A percentage[TT]=0
+declare  -A singlet=(['HEAD']=0 ['TAIL']=0 )
+declare -A doublet=( ["HH"]=0 ["TH"]=0 ["TH"]=0 ["TT"]=0 )
+declare -A doubletpercentage=( ["HH"]=0 ["TH"]=0 ["TH"]=0 ["TT"]=0 )
+declare -A triplet=( ["HHH"]=0 ["HHT"]=0 ["HTH"]=0 ["THH"]=0 \
+          ["HTT"]=0 ["TTT"]=0 ["TTH"]=0 ["THT"]=0 )
+declare -A tripletpercentage=( ["HHH"]=0 ["HHT"]=0 ["HTH"]=0 ["THH"]=0 \
+          ["HTT"]=0 ["TTT"]=0 ["TTH"]=0 ["THT"]=0 )
 singlets(){
     for (( i = 0 ; i < $1 ; i++ ))  ; do
         if [[ $((RANDOM%2)) -eq '1' ]]; then
@@ -22,8 +19,9 @@ singlets(){
  }
 
 doublets(){
+    total=$1
      local previous=$((RANDOM%2))
-        for (( i = 0 ; i < $1 ; i++ ))  ; do
+        for (( i = 0 ; i < $((total -1)) ; i++ ))  ; do
             local present=$((RANDOM%2))
              if [[ $previous -eq '1' && $present -eq '1' ]]; then
                  ((doublet[HH]++))
@@ -35,16 +33,64 @@ doublets(){
                 ((doublet[TT]++))
         fi
         previous=$present
-done
-total=$1
+        done
+
 hh=${doublet[HH]}
-percentage[HH]=$(((hh * 100)/total))
+doubletpercentage[HH]=$(((hh * 100)/total))
 ht=${doublet[HT]}
-percentage[HH]=$(((ht * 100)/total))
+doubletpercentage[HH]=$(((ht * 100)/total))
 th=${doublet[TH]}
-percentage[HH]=$(((th * 100)/total))
+doubletpercentage[HH]=$(((th * 100)/total))
 tt=${doublet[TT]}
-percentage[HH]=$(((tt * 100)/total))
+doubletpercentage[HH]=$(((tt * 100)/total))
+ }
+
+ triplets(){
+     total=$1
+     local previous2=$((RANDOM%2))
+     local previous1=$((RANDOM%2))
+       for (( i = 0 ; i < $1 ; i++ )); do
+            local present=$((RANDOM%2))
+             if [[ $previous2 -eq '1' && $previous1 -eq '1' && $present -eq '1' ]]; then
+                 ((triplet[HHH]++))
+            elif [[ $previous2 -eq '1' && $previous1 -eq '1' && $present -eq '0' ]]; then
+                 ((triplet[HHT]++))
+            elif [[ $previous2 -eq '1' && $previous1 -eq '0' && $present -eq '1' ]]; then
+                 ((triplet[HTH]++))
+            elif [[ $previous2 -eq '0' && $previous1 -eq '1' && $present -eq '1' ]]; then
+                 ((triplet[THH]++))
+            elif [[ $previous2 -eq '1' && $previous1 -eq '0' && $present -eq '0' ]]; then
+                 ((triplet[HTT]++))
+            elif [[ $previous2 -eq '0' && $previous1 -eq '1' && $present -eq '0' ]]; then
+                 ((triplet[THT]++))
+            elif [[ $previous2 -eq '0' && $previous1 -eq '0' && $present -eq '1' ]]; then
+                 ((triplet[TTH]++))
+            elif [[ $previous2 -eq '0' && $previous1 -eq '0' && $present -eq '0' ]]; then
+                ((triplet[TTT]++))
+        fi
+        echo $previous2 $previous1 $present 
+        previous2=$previous1
+        previous1=$present
+        done
+
+hhh=${triplet[HHH]}
+tripletpercentage[HHH]=$(((hhh * 100)/total))
+hht=${triplet[HHT]}
+tripletpercentage[HHT]=$(((hht * 100)/total))
+thh=${triplet[THH]}
+tripletpercentage[THH]=$(((thh * 100)/total))
+hth=${triplet[HTH]}
+tripletpercentage[HTH]=$(((hth * 100)/total))
+tth=${triplet[TTH]}
+tripletpercentage[TTH]=$(((tth * 100)/total))
+htt=${triplet[HTT]}
+tripletpercentage[HTT]=$(((htt * 100)/total))
+tht=${triplet[THT]}
+tripletpercentage[THT]=$(((tht * 100)/total))
+ttt=${triplet[TTT]}
+tripletpercentage[TTT]=$(((ttt * 100)/total))
+
+
  }
 
 #Calling method to find the singlets
@@ -52,6 +98,11 @@ singlets $flips
 
 #Calling method to find the combination of doublets
 doublets $flips
+
+#calling method to find the combination of triplets
+triplets $flips
+
+
 
 
  
