0eb39f7fb75f1acb4e69fbac8ea2d3c19080a3d1
kyoolbaba <kyoolbaba@gmail.com>
2020-04-13 17:11:49 +0530

    Adding Feature to store singlet  head/tail

--- flipCoin.sh
+++ flipCoin.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-face=$((RANDOM%2))
+ echo Enter the number of flips
+ read flips
+ declare  -A singlet[HEAD]=0
+ declare -A  singlet[TAIL]=0
+
+ singlets(){
+for (( i = 0 ; i < $1 ; i++ ))  ; do
+        if [[ $((RANDOM%2)) -eq '1' ]]; then
+            ((singlet[HEAD]++))
+        else
+            ((singlet[TAIL]++))
+        fi
+done
+ }
+
+ singlets $flips
+
+
+
